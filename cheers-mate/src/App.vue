1af75754dccdf58daee7dc8fdeba877d678ed8b6
Juval Bechar <JuvalB996@gmail.com>
2018-11-27 11:15:45 +0200

    nav bar

--- cheers-mate/src/App.vue
+++ cheers-mate/src/App.vue
@@ -1,24 +1,16 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/cheer">Cheer Details</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/map">Map</router-link>
-=======
-    <div id="nav" class="flex">
+    <div id="nav" class="flex space-between">
        <h1>Cheer App</h1>
        <div class="links">
         <router-link to="/">Home</router-link> |
         <router-link to="/cheer">Cheers</router-link> |
         <router-link to="/about">About</router-link> 
       </div>
-      <div>
+      <!-- <div>
         <router-link to="/signup">Sign up</router-link> |
         <router-link to="/login">Log in</router-link>
-      </div>
->>>>>>> 03d3fb957485bdbd25ba187937d34930771ce8d0
+      </div> -->
     </div>
     <router-view/>
   </div>
