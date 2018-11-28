28186af572efa1b24ec6d5f9cb5d79b6af9c1c13
Adi Baram <adididi@gmail.com>
2018-11-28 14:07:52 +0200

    merge

--- cheers-mate/src/services/cheer-service.js
+++ cheers-mate/src/services/cheer-service.js
@@ -18,13 +18,8 @@ export default {
 }
 
 function _createCheers() {
-<<<<<<< HEAD
-    return [_createCheer(1545908790000, {lat: 30, lng: -30}, [{user:'A'}, {user:'B'}],'lorem',4,(Math.random() > 0.5)? 'en' : 'he'),
-           _createCheer(1543921590000, {lat: 30, lng: -40}, [{user:'A'}, {user:'B'}],'lorem',4, (Math.random() > 0.5)? 'en' : 'he'),];
-=======
     return [_createCheer(Date.now(), {lat: 32.0714143, lng: 34.78723}, [{user:'A'}, {user:'B'}],'lorem',4,(Math.random() > 0.5)? 'en' : 'he'),
            _createCheer(Date.now(), {lat: 30, lng: -40}, [{user:'A'}, {user:'B'}],'lorem',4, (Math.random() > 0.5)? 'en' : 'he'),];
->>>>>>> 67a1028e97914219f55ca6d8f9006b7ebce58677
 }
 
 function _createCheer(date,position,attendance,desc,spots,language) {
