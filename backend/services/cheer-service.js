8d30a1162ba4596366e2edbe2c476c4bf42a8d19
Adi Baram <adididi@gmail.com>
2018-11-29 16:17:39 +0200

    merge

--- backend/services/cheer-service.js
+++ backend/services/cheer-service.js
@@ -54,18 +54,11 @@ function queryRadius(params) {
     }
     console.log('DEBUG::locationFilter', locationFilter);
     return mongoService.connect()
-<<<<<<< HEAD
-        .then(db => {
-            const collection = db.collection(COLLECTION_NAME);
-            return collection.find(params).toArray();
-        })
-=======
     .then(db => {
         const collection = db.collection('cheer');
         collection.createIndex({ 'position': "2dsphere" });
         return collection.find(locationFilter).toArray();
     })
->>>>>>> create
 }
 // GET SPECIFIC CHEER
 function getById(cheerId) {
