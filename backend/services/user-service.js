9c18aa1635504717ab9b1531f2e5780b7fdb5ea3
OrpazL <orpaz.lazar@gmail.com>
2018-11-30 20:17:14 +0200

    user server

--- backend/services/user-service.js
+++ backend/services/user-service.js
@@ -2,34 +2,47 @@ const mongoService = require('./mongo-service')
 
 const ObjectId = require('mongodb').ObjectId;
 
+const COLLECTION_NAME = 'user';
 
-function checkLogin({ nickname }) {
+function checkLogin({nickname, password}) {
     return mongoService.connect()
-        .then(db => db.collection('user').findOne({ nickname }))
+        .then(db => db.collection(COLLECTION_NAME).findOne({ nickname }))
+        .then(user=>{
+            if (user.password === password) return user;
+            else throw new Error('password does not match');
+        })
 }
-
 
 function getById(id) {
-    const _id = new ObjectId(id)
+    var _id = new ObjectId(id);
     return mongoService.connect()
-        .then(db => db.collection('user').findOne({ _id }))
+        .then(db => db.collection(COLLECTION_NAME).findOne({ _id }))
+        .then(({nickname,email})=> {
+            console.log('DEBUG::nickname', nickname);
+            return {nickname,email}}
+            )
 }
-
-
 
 function query() {
     return mongoService.connect()
-        .then(db => db.collection('user').find({}).toArray())
+        .then(db => db.collection(COLLECTION_NAME).find({}).toArray())
 }
 
 // todo  - add user only if nickname is not taken
-function addUser({ nickname }) {
-    var user = { nickname }
+function add(user) {
     return mongoService.connect()
-        .then(db => db.collection('user').insertOne(user))
+        .then(db => db.collection(COLLECTION_NAME).insertOne(user))
         .then(res => {
-            user._id = res.insertedId
-            return user
+            console.log('DEBUG:add user:res', res);
+        })
+}
+
+function remove(userId) {
+    userId = new ObjectId(userId)
+    return mongoService.connect()
+        .then(dbConn => {
+            const userCollection = dbConn.collection(COLLECTION_NAME);
+            return userCollection.remove({ _id: userId })
         })
 }
 
@@ -42,6 +55,7 @@ function addUser({ nickname }) {
 module.exports = {
     query,
     getById,
-    addUser,
+    add,
+    remove,
     checkLogin
 }
